888f462fb46dfa5fa6df6fc98df2028518b3162c
Efecan Açıkgöz <132709251+hexonduty@users.noreply.github.com>
2023-06-04 23:29:22 +0300

    Add files via upload

---
+++ ArpetaYazılım/countup.js
@@ -0,0 +1,9 @@
+const countTitle1 = new countUp.CountUp('countTitle1', 27);
+const countTitle2 = new countUp.CountUp('countTitle2', 60);
+const countTitle3 = new countUp.CountUp('countTitle3', 5);
+const countTitle4 = new countUp.CountUp('countTitle4', 97);
+
+countTitle1.start();
+countTitle2.start();
+countTitle3.start();
+countTitle4.start();
